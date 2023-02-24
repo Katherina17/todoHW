7306611ddec39c2cdd6e9da58e72f961f1f61a44
katherina17 <katherinepetrova76@gmail.com>
2023-02-24 11:54:39 +0300

    feat: style the thumb

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {alpha, Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ maxWidth: '147px', color: '#00CC22'// стили для слайдера // пишет студент
-                
-            }}
-            aria-labelledby="non-linear-slider"
+            sx={{ maxWidth: '147px',
+                color: '#00CC22',
+                '& .MuiSlider-rail': {backgroundColor: '#8B8B8B'},
+                '& .MuiSlider-thumb': {
+                    height: 24,
+                    width: 24,
+                    backgroundColor: '#fff',
+                    border: '2px solid currentColor',
+                    '&:after': {
+                        width: '10px',
+                        height: '10px',
+                        display: 'block',
+                        background: '#00CC22',
+                        borderRadius: '50%',
+                    },
 
+            }}}
+            aria-labelledby="non-linear-slider"
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
